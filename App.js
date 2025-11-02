643f6aa664987e75e7c4fe7c23a389c7a5f69852
shivam-srivas <srivastava.shivam25061995@gmail.com>
2025-11-02 19:33:49 +0530

    About jSX

--- App.js
+++ App.js
@@ -1,37 +1,12 @@
-const parent = React.createElement(
-    "div",
-    {id: "parent"},[
-    
-        React.createElement(
-            "div",
-            {id: "child"},[
-            React.createElement("h1"
-            , {} , "I am an h1 tag"),
-             React.createElement("h2"
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-            , {} , "I am an h2 tag"),]
-        ),React.createElement(
-            "div",
-            {id: "child2"},[
-            React.createElement("h1"
-            , {} , "I am an h1 tag"),
-             React.createElement("h2"
+// JSX
+const heading = (
+  <h1 className="head" tabIndex="5">
+    Namaste React Using JSX
+  </h1>
+);
 
-            , {} , "I am an h2 tag"),]
-        ),
-
-    
-]);
-
-
-
-const heading = React.createElement("h1",
-     {id:"heading", xyz:"abc"}, 
-     "Hello W from React")
-
-console.log(heading)
-
-   const root = ReactDOM.createRoot(document.getElementById('root'));
- root.render(parent);
-
- 
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
